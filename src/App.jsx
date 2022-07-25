28ed8e3c84ac6a65f96d7ed306017073383dc8a5
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-24 19:22:57 -0700

    Added Loading Screen In Between Transitions

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,46 @@ import { Header, Footer, ContactPage, HomePage, ProfilePage, SignInPage, AboutPa
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './config/firebase';
 import './App.css'
-
+import GridLoader from 'react-spinners/GridLoader'
 function App() {
   const [user, loading, error] = useAuthState(auth);
+  const [ load, setLoad] = React.useState(false);
 
+  React.useEffect(()=>{
+    setLoad(true);
+    setTimeout(()=>{
+      setLoad(false);
+    }, 750);
+  }, []);
 
   return (
     <div className="App">
+    { load ? (
+      <div className='flex justify-center items-center h-screen'>
+        <GridLoader size={30} color={'#8AA3F3'} loading={load} />
+      </div>
+    ) : (
+      <div>
         <Header />
-        <Routes>
-          {user ? 
-          <Route path="/" element={<UserHome />} />
-          :
-          <Route path="/" element={<HomePage />} />
-          }
-          <Route path="/contact" element={<ContactPage />} />
-          {user ? 
-          <Route path="/profile" element={<ProfilePage />} />
-          :
-          <Route path="/profile" element={<SignInPage />} />
-          }
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/faq" element={<FAQ />} />
-        </Routes>
-        <Footer />
+          <Routes>
+            {user ? 
+            <Route path="/" element={<UserHome />} />
+            :
+            <Route path="/" element={<HomePage />} />
+            }
+            <Route path="/contact" element={<ContactPage />} />
+            {user ? 
+            <Route path="/profile" element={<ProfilePage />} />
+            :
+            <Route path="/profile" element={<SignInPage />} />
+            }
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/faq" element={<FAQ />} />
+          </Routes>
+          <Footer />
+        </div>
+      )
+    }
       </div>
   )
 }
