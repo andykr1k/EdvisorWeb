737ae9fd2025e5742e8cede565881afff85132f7
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-02 14:04:09 -0700

    Not Migrating Fixed Issue

--- src/components/AddCourseForm.jsx
+++ src/components/AddCourseForm.jsx
@@ -1,28 +1,28 @@
 import * as React from 'react'
 import { auth, firestore, db } from '../config/firebase'
 import { writeBatch, setDoc, collection, addDoc, doc } from 'firebase/firestore';
+
 function AddCourseForm() {
     const courseUserPath = "users/" + auth.currentUser.uid
     const courseQueryPath = "users/" + auth.currentUser.uid + "/courses/";
-    const courseQuery = firestore.collection(courseQueryPath);
-    const dbRef = collection(db, courseQueryPath);
-
+    const coursesRef = firestore.collection('users').doc(auth.currentUser.uid).collection('courses');
     var [courseName, setCourseName] = React.useState('')
     var [courseAbbrev, setCourseAbbrev] = React.useState('')
     var [courseGpa, setCourseGpa] = React.useState('')
     var [courseUnits, setCourseUnits] = React.useState('')
 
-     const WriteCourseData = async(name, abbrev, gpa, units, path) => {
+     const WriteCourseData = async(e) => {
+      e.preventDefault();
       try {
-        await setDoc(doc(db, courseQueryPath, "test2"), {
-          name: name, 
-          abbrev: abbrev,
-          units: units,
-          gpa: gpa,
+        await coursesRef.add({
+          name: courseName, 
+          abbrev: courseAbbrev,
+          units: courseUnits,
+          gpa: courseGpa,
           current: true,
           complete: false,
-          path: path
-        });
+          path: false
+        })
         console.log("Document written with ID: ");
       } catch (e) {
         console.error("Error adding document: ", e);
@@ -36,7 +36,7 @@ function AddCourseForm() {
 
   return (
     <div className=''>
-        <form className='grid' onSubmit={WriteCourseData(courseName, courseAbbrev, courseGpa, courseUnits, false)}>
+        <form className='grid' onSubmit={WriteCourseData}>
             <input className='w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded' id="name" placeholder='Course Name' onChange={ (e) => setCourseName(e.target.value) }></input>
             <input className='w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded' id="id" placeholder='Course ID' onChange={ (e) => setCourseAbbrev(e.target.value) }></input>
             <input className='w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded' id="units" placeholder='Course Units' onChange={ (e) => setCourseUnits(e.target.value) }></input>
