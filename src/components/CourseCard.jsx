8a6221ed3c07d9b844e360f4bbed32175fef1b5c
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-07 14:13:19 -0700

    Fixed Deleting Error

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -2,17 +2,27 @@ import * as React from 'react'
 import { doc, deleteDoc, collection } from "firebase/firestore";
 import { auth, firestore, db } from '../config/firebase'
 import { Button, useToast } from '@chakra-ui/react'
+import { parsePath } from 'react-router-dom';
 
 function CourseCard(props) {
 const toast = useToast();
 const { gpa, name, abbrev, units } = props;
-const pathString = "users/" + auth.currentUser.uid + '/courses'
+const pathString = "users/" + auth.currentUser.uid + "/courses"
 const coursesRef = firestore.collection(pathString);
 
-const deleteDocument = () => {
-  firebase.firestore().doc('users/rN8mTD2YX5g1W8RGZwU1EsAzaD03/courses/W7iXxYhCjOtc6gjBMavj').delete();
-  console.log("Document written with ID: ");
+const deleteDocument = async (id) => {
+  const userCourses = doc(db, pathString, id)
+  await deleteDoc(userCourses);
+  toast({
+    title: 'Course Deleted.',
+    description: "We've deleted your course.",
+    status: 'warning',
+    duration: 9000,
+    isClosable: true,
+  });
+  console.log("Deleted course");
 }
+
   return (
     <div className=' m-2 shrink-0'>
       
@@ -33,15 +43,7 @@ const deleteDocument = () => {
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                 />
             </svg>
-            <Button colorScheme='teal' height={7} width={2} variant='link' onClick={() =>
-              toast({
-                title: 'Course Deleted.',
-                description: "We've deleted your course.",
-                status: 'warning',
-                duration: 9000,
-                isClosable: true,
-              })
-            }>
+            <Button colorScheme='teal' height={7} width={2} variant='link' onClick={() => deleteDocument('d5PPiR6keht99HTqqL7E')}>
               -
             </Button>
           </div>
