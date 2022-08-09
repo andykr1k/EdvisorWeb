61233f15e9ac058df3db5bb715d37edb7b3c6e24
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-08 20:06:13 -0700

    Fixing UI

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -24,7 +24,7 @@ const deleteDocument = async (id) => {
 }
 
   return (
-    <div>
+    <div className='m-2 shrink-0'>
       <motion.button 
         className='m-2 shrink-0' 
         whileHover={{ scale: 0.9 }}
