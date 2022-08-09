0962afa5c39609ae6c4e8e947edaf6de0362b0f6
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-08 20:03:20 -0700

    Fixing UI

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { doc, deleteDoc, collection } from "firebase/firestore";
 import { auth, firestore, db } from '../config/firebase'
-import { Button, useToast } from '@chakra-ui/react'
-
+import { useToast, Modal, ModalOverlay, ModalBody, ModalContent, ModalCloseButton, ModalFooter, ModalHeader, useDisclosure, Button} from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 function CourseCard(props) {
 const toast = useToast();
 const { gpa, name, abbrev, units } = props;
 const pathString = "users/" + auth.currentUser.uid + "/courses"
 const coursesRef = firestore.collection(pathString);
+const { isOpen, onOpen, onClose } = useDisclosure()
 
 const deleteDocument = async (id) => {
   const userCourses = doc(db, pathString, id)
@@ -23,55 +24,102 @@ const deleteDocument = async (id) => {
 }
 
   return (
-    <div className='m-2 shrink-0'>
-      
-        <div class="relative block p-3 border border-gray-100 shadow-md rounded-xl">
-          <div className="flex justify-between items-center">
-          <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-5 h-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="grey"
-                stroke-width="2"
-                >
-                <path d="M12 14l9-5-9-5-9 5 9 5z" />f
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                />
-            </svg>
-            <Button 
-              colorScheme='red' 
-              size='xs'
-              variant='ghost' 
-              onClick={() => deleteDocument(abbrev)}
-              className="text-center align-middle"
-            >
-              -
-            </Button>
-          </div>
-            
-        <div class="mt-2 text-gray-500">
+    <div>
+      <motion.button 
+        className='m-2 shrink-0' 
+        whileHover={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        onClick={onOpen}
+      >
+      <div class="relative block p-3 border border-gray-100 shadow-md rounded-xl">
+        <div className="flex justify-between items-center">
+        <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="grey"
+              stroke-width="2"
+              >
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />f
+              <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              />
+          </svg>
+          <Button 
+            colorScheme='red' 
+            size='xs'
+            variant='ghost' 
+            onClick={() => deleteDocument(abbrev)}
+            className="text-center align-middle"
+          >
+            -
+          </Button>
+        </div>
+          
+      <div class="mt-2 text-gray-500">
 
-            <h5 class="mt-2 text-xl font-bold text-gray-900">{name}</h5>
+          <h5 class="mt-2 text-xl font-bold text-gray-900">{name}</h5>
+          <div>
+              <p class="mt-2 text-sm">
+              {abbrev}
+              </p>
+          </div>
+      </div>
+      <div className='flex mt-2 space-x-1'>
+          <div class="rounded-md p-2 bg-green-100 text-green-600 font-medium text-xs">
+          GPA: {gpa}
+          </div>
+          <div class="rounded-md p-2 bg-orange-100 text-orange-600 font-medium text-xs">
+          Units: {units}
+          </div>
+      </div>
+      </div>
+    </motion.button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            {name}
             <div>
                 <p class="mt-2 text-sm">
                 {abbrev}
                 </p>
             </div>
-        </div>
-        <div className='flex mt-2 space-x-1'>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          
+          <div className='flex mt-2 space-x-1'>
             <div class="rounded-md p-2 bg-green-100 text-green-600 font-medium text-xs">
             GPA: {gpa}
             </div>
             <div class="rounded-md p-2 bg-orange-100 text-orange-600 font-medium text-xs">
             Units: {units}
             </div>
-        </div>
-        </div>
+          </div>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Edit
+            </Button>
+            <Button 
+            colorScheme='red' 
+            variant='outline' 
+            onClick={() => deleteDocument(abbrev)}
+            className="text-center align-middle"
+          >
+            Delete
+          </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
+    
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,9 +14,11 @@ function Header() {
                 <div class="flex items-center ">
                     <div class="flex items-center justify-between flex-1 gap-8 ">
                         <div class="flex gap-4">
-                        <a
+                        <motion.a
                             href="/"
                             class="block p-2.5 text-gray-600 bg-white rounded-lg hover:text-gray-700 shrink-0 shadow-sm"
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ type: "spring", stiffness: 400, damping: 10 }} 
                         >
                             <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -30,11 +32,13 @@ function Header() {
                                 d="M18.121,9.88l-7.832-7.836c-0.155-0.158-0.428-0.155-0.584,0L1.842,9.913c-0.262,0.263-0.073,0.705,0.292,0.705h2.069v7.042c0,0.227,0.187,0.414,0.414,0.414h3.725c0.228,0,0.414-0.188,0.414-0.414v-3.313h2.483v3.313c0,0.227,0.187,0.414,0.413,0.414h3.726c0.229,0,0.414-0.188,0.414-0.414v-7.042h2.068h0.004C18.331,10.617,18.389,10.146,18.121,9.88 M14.963,17.245h-2.896v-3.313c0-0.229-0.186-0.415-0.414-0.415H8.342c-0.228,0-0.414,0.187-0.414,0.415v3.313H5.032v-6.628h9.931V17.245z M3.133,9.79l6.864-6.868l6.867,6.868H3.133z"
                             />
                             </svg>
-                        </a>
+                        </motion.a>
 
-                        <a
+                        <motion.a
                             href="/courses"
                             class="block p-2.5 text-gray-600 bg-white rounded-lg hover:text-gray-700 shrink-0 shadow-sm"
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ type: "spring", stiffness: 400, damping: 10 }} 
                         >
                             <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -49,11 +53,13 @@ function Header() {
                                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                             />
                             </svg>
-                        </a>
+                        </motion.a>
 
-                        <a
+                        <motion.a
                             href="/notifications"
                             class="block p-2.5 text-gray-600 bg-white rounded-lg hover:text-gray-700 shrink-0 shadow-sm"
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ type: "spring", stiffness: 400, damping: 10 }} 
                         >
                             <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -67,7 +73,7 @@ function Header() {
                                 d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                             />
                             </svg>
-                        </a>
+                        </motion.a>
                         <SettingsModal />
                         </div>
 
@@ -76,9 +82,11 @@ function Header() {
                         class="flex items-center transition rounded-lg group shrink-0 "
                         >
                         <a href="/profile" class=" rounded-full shadow-md">
-                        <img
+                        <motion.img
                             class="object-cover w-10 h-10 rounded-full"
                             src={user.photoURL}
+                            whileHover={{ scale: 1.2 }}
+                            transition={{ type: "spring", stiffness: 400, damping: 10 }}
                         />
                         </a>
 
