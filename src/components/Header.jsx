ecd3f16663ee83161a664d75b9da6c7ce2612934
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-14 14:29:39 -0700

    Fixing Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     const [user, loading, error] = useAuthState(auth);
 
   return (
-    <header class="bg-gray-50/90 shadow-lg">
+    <header class="bg-gray-50/80 shadow-lg">
         <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8"> 
             { user ? 
                 <div class="flex items-center ">
