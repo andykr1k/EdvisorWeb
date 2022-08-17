df03d2a2d71536a5a7ae9be1f3fb9a6e788ee405
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-16 17:52:50 -0700

    About Page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,14 +4,11 @@ function AboutPage() {
 
   return (
     <div>
-      
+      <h2>
+      After engaging in the tumultuous transfer process, Andrew Krikorian and Sahara Williams realized that a platform to simplify the journey from community college to university was long overdue. They believe that every student should have their college journey at their fingertips in order to stay on track and keep up with the necessities that are destined to get mismanaged within our outdated transfer process. With this in mind, the seamless user platform Edvisor was born.
+      </h2>
     </div>
   )
 }
 
 export default AboutPage
-
-/*
-
-After engaging in the tumultuous transfer process, Andrew Krikorian and Sahara Williams realized that a platform to simplify the journey from community college to university was long overdue. They believe that every student should have their college journey at their fingertips in order to stay on track and keep up with the necessities that are destined to get mismanaged within our outdated transfer process. With this in mind, the seamless user platform Edvisor was born.
-*/
