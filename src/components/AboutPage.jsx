3354d504bf6d857a2a0876f810aba0d3cad6c82d
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-17 00:12:57 -0700

    Team and About Page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,12 +4,39 @@ function AboutPage() {
 
   return (
     <div className="m-10 text-lg space-y-3 bg-slate-500 bg-opacity-5 rounded-lg shadow-lg p-3">
-      <div className="grid place-items-center">
-      <h1>About Us</h1>
-        <h2>
-        After engaging in the tumultuous transfer process, Andrew Krikorian and Sahara Williams realized that a platform to simplify the journey from community college to university was long overdue. They believe that every student should have their college journey at their fingertips in order to stay on track and keep up with the necessities that are destined to get mismanaged within our outdated transfer process. With this in mind, the seamless user platform Edvisor was born.
-        </h2>
-      </div>
+      <section>
+        <div class="max-w-screen-xl p-5">
+          <div class="max-w-3xl">
+            <h2 class="text-3xl font-bold sm:text-4xl">
+              About Us
+            </h2>
+          </div>
+
+          <div class="grid grid-cols-1 gap-8 mt-8 lg:gap-16 lg:grid-cols-2">
+            <div class="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+              <img
+                class="absolute inset-0 object-cover w-full h-full"
+                src="https://www.hyperui.dev/photos/man-1.jpeg"
+                alt="Man using a computer"
+              />
+            </div>
+
+            <div class="lg:py-16">
+              <article class="space-y-4 text-gray-600">
+                <p>
+                After engaging in the tumultuous transfer process, Andrew Krikorian 
+                and Sahara Williams realized that a platform to simplify the journey 
+                from community college to university was long overdue. They believe 
+                that every student should have their college journey at their fingertips 
+                in order to stay on track and keep up with the necessities that are 
+                destined to get mismanaged within our outdated transfer process. With 
+                this in mind, the seamless user platform Edvisor was born.
+                </p>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -4,12 +4,51 @@ function TeamPage() {
 
   return (
     <div className="m-10 text-lg space-y-3 bg-slate-500 bg-opacity-5 rounded-lg shadow-lg p-3">
-    <div className="grid place-items-center">
-        <h1>Team</h1>
-        
-        <h2>
-            Co-Founders - Sahara Williams and Andrew Krikorian
-        </h2>
+      <div class="grid place-items-center ">
+          <p class="text-center text-3xl font-bold text-gray-800">
+              Our Team
+          </p>
+          <p class="text-center mb-12 text-xl font-normal text-gray-500">
+              Edvisor
+          </p>
+          <div class="flex items-center flex-col md:flex-row justify evenly">
+              <div class="p-4">
+                  <div class="text-center mb-4 opacity-90">
+                      <a href="#" class="block relative">
+                          <img alt="profile" src="/images/person/1.jpg" class="mx-auto object-cover rounded-full h-40 w-40 "/>
+                      </a>
+                  </div>
+                  <div class="text-center">
+                      <p class="text-2xl text-gray-800 ">
+                          Sahara Williams
+                      </p>
+                      <p class="text-xl text-gray-500  font-light">
+                          Co-Founder
+                      </p>
+                      <p class="text-md text-gray-500 max-w-xs py-4 font-light">
+                      BIO
+                      </p>
+                  </div>
+              </div>
+              <div class="p-4">
+                  <div class="text-center mb-4 opacity-90">
+                      <a href="#" class="block relative">
+                          <img alt="profile" src="/images/person/4.jpg" class="mx-auto object-cover rounded-full h-40 w-40 "/>
+                      </a>
+                  </div>
+                  <div class="text-center">
+                      <p class="text-2xl text-gray-800 ">
+                          Andrew Krikorian
+                      </p>
+                      <p class="text-xl text-gray-500">
+                          Co-Founder/Engineer
+                      </p>
+                      <p class="text-md text-gray-500 max-w-xs py-4 font-light">
+                      BIO
+                      </p>
+                  </div>
+              </div>
+          </div>
       </div>
     </div>
   )
