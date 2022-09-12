edb308957aeb8d5d4d8408d1a50becc1811b2c61
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-09-11 20:30:33 -0700

    Changing Course Section Set Up

--- src/components/CourseSection.jsx
+++ src/components/CourseSection.jsx
@@ -11,13 +11,8 @@ function CourseSection() {
 
     const pathString = "users/" + auth.currentUser.uid + '/courses'
     const coursesRef = firestore.collection(pathString);
-    const completeQuery = coursesRef.where("complete", "==", true);
     const currentQuery = coursesRef.where("current", "==", true);
-    const pathQuery = coursesRef.where("path", "==", true);
-
-    const [completedCourses] = useCollectionData(completeQuery, { idField: 'abbrev' });
     const [currentCourses] = useCollectionData(currentQuery, { idField: 'abbrev' });
-    const [pathCourses] = useCollectionData(pathQuery, { idField: 'abbrev' });
 
   return (
     <div className='grid m-auto bg-slate-500 bg-opacity-5 rounded-lg shadow-lg p-2'>
@@ -31,20 +26,52 @@ function CourseSection() {
                 {currentCourses && currentCourses.map(course => <CourseCard gpa={course.gpa} name={course.name} abbrev={course.abbrev} units={course.units}/>)}
             </div>
 
-            <h2 className='text-slate-500 text-xl md:text-3xl  m-3'>Completed Courses</h2>
-
-            <div className='flex flex-shrink-0 overflow-x-auto'>
-            {completedCourses && completedCourses.map(course => <CourseCard gpa={course.gpa} name={course.name} abbrev={course.abbrev} units={course.units}/>)}
-            </div>
-
-            <h2 className='text-slate-500 text-xl md:text-3xl  m-3'>Your Path</h2>
-
-            <div className='flex flex-shrink-0 overflow-x-auto'>
-            {pathCourses && pathCourses.map(course => <CourseCard gpa={course.gpa} name={course.name} abbrev={course.abbrev} units={course.units}/>)}
-            </div>
+          <CompletedSection/>
         </div>
     </div>
   )
+}
+
+function CompletedSection() {
+
+  const pathString = "users/" + auth.currentUser.uid + '/courses'
+  const coursesRef = firestore.collection(pathString);
+  const completeQuery = coursesRef.where("complete", "==", true);
+
+  const [completedCourses] = useCollectionData(completeQuery, { idField: 'abbrev' });
+
+return (
+  <>
+      <div className='flex-shrink-0 overflow-x-auto max-w-screen'>
+
+          <h2 className='text-slate-500 text-xl md:text-3xl  m-3'>Completed Courses</h2>
+
+          <div className='flex flex-shrink-0 overflow-x-auto'>
+          {completedCourses && completedCourses.map(course => <CourseCard gpa={course.gpa} name={course.name} abbrev={course.abbrev} units={course.units}/>)}
+          </div>
+      </div>
+  </>
+)
+}
+
+function PathSection() {
+
+  const pathString = "users/" + auth.currentUser.uid + '/courses'
+  const coursesRef = firestore.collection(pathString);
+  const pathQuery = coursesRef.where("path", "==", true);
+  const [pathCourses] = useCollectionData(pathQuery, { idField: 'abbrev' });
+
+return (
+  <>
+      <div className='flex-shrink-0 overflow-x-auto max-w-screen'>
+          <h2 className='text-slate-500 text-xl md:text-3xl  m-3'>Your Path</h2>
+
+          <div className='flex flex-shrink-0 overflow-x-auto'>
+          {pathCourses && pathCourses.map(course => <CourseCard gpa={course.gpa} name={course.name} abbrev={course.abbrev} units={course.units}/>)}
+          </div>
+      </div>
+  </>
+)
 }
 
 export default CourseSection
